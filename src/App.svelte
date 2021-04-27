4859900a72d927635fb1cbb3842d47d3afe756d1
Joseph Allain <joseph.allain24@gmail.com>
2021-04-28 00:40:10 +0200

    analytics

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
 	import firebase from 'firebase/app';
 	import 'firebase/firestore';
 	import 'firebase/auth';
+	import 'firebase/analytics';
 	import {users as alumnis2} from './ocazou';
 	import {fade} from 'svelte/transition';
 	import AlumniEdit from './components/AlumniEdit.svelte';
@@ -15,8 +16,10 @@
 		storageBucket: 'alumni-simplon.appspot.com',
 		messagingSenderId: '52608283217',
 		appId: '1:52608283217:web:79fc793d9e14a93c6afe70',
+		measurementId: 'G-BYZVWE9CGE',
 	};
 	firebase.initializeApp(firebaseConfig);
+	firebase.analytics();
 	const db = firebase.firestore();
 	const auth = firebase.auth();
 
@@ -64,7 +67,7 @@
 		return Object.keys(alumni.contact).filter((k) => {
 			console.log(k);
 			console.log(publicContactProps.includes(k) && alumni.contact[k]?.length > 0);
-			return publicContactProps.includes(k) && (alumni.contact[k]?.length > 0);
+			return publicContactProps.includes(k) && alumni.contact[k]?.length > 0;
 		}).length;
 	}
 </script>
